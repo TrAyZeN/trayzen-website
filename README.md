8109700ee2f996c9eba5cd3f3dfa717f0940fe6c
TrAyZeN <1810leo@gmail.com>
2019-09-25 18:27:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1 align="center">
+    trayzen-website
+</h1>
+
+> TrAyZeN's personal website
+<div align="center">
+    <img src="assets/image.png"/>
+</div>
+Longer description of the project
+
+## Requirements
+- [Node](https://nodejs.org/)
+
+## Install
+```
+git clone https://github.com/TrAyZeN/trayzen-website.git
+cd trayzen-website
+yarn install
+```
+
+## License
+MIT TrAyZeN
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h4>TrAyZeN</h4>
+        <p className="Description">
+          I love to code and to play with maths.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
