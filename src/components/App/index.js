9c3ea43ae0495bc811695b59084aa6aa5654b56a
TrAyZeN <1810leo@gmail.com>
2020-01-24 21:34:31 +0100

    Change positioning and add link

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import logo from '../../assets/logo.png';
 import './style.scss';
+
+import profile_picture from '../../assets/profile_picture.png';
+import github from '../../assets/github.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h4>TrAyZeN</h4>
-        <p className="Description">
-          I love to code and to play with maths.
+        <img src={profile_picture} className="Profile-picture" alt="profile_picture" />
+
+        <h1>TrAyZeN</h1>
+
+        <p className="Status">
+          Self taught programmer addicted to learning
         </p>
+
+        <hr className="Vertical-line" />
+
+        <a href="https://github.com/TrAyZeN">
+          <img src={github} className="Social-icon" />
+        </a>
+
+        {/* <p className="Description">
+          I love to code and to play with maths.
+        </p> */}
+
       </header>
     </div>
   );
