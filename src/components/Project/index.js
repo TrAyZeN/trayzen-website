48989ecf3ace2901e9e0367bbd28439dedd7c89b
TrAyZeN <1810leo@gmail.com>
2020-06-11 01:31:19 +0200

    Use react hooks

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,46 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import fetch from 'node-fetch';
 import './style.scss';
 
-class Project extends React.Component {
-  constructor(props) {
-    super(props);
-
-    const { user, name } = props;
-    this.state = {
-      user,
-      name,
-      description: '',
-      htmlUrl: '',
-    };
-
-    this.parseRepo(this.state);
-  }
-
-  parseRepo({ user, name }) {
+const getRepositoryInformation = (user, name) =>
+  new Promise((resolve, reject) => {
     fetch(`https://api.github.com/repos/${user}/${name}`)
-      .then((response) => response.json())
-      .then((response) =>
-        this.setState({
-          response,
-          htmlUrl: response.html_url,
-        })
-      );
-  }
+      .then((response) => resolve(response.json()))
+      .catch((error) => reject(error));
+  });
 
-  render() {
-    const { htmlUrl, name, description } = this.state;
-    return (
-      <div className="Project">
-        <a href={htmlUrl} target="_blank" rel="noopener noreferrer">
-          <button type="button">
-            <h2>{name}</h2>
-            <p>{description}</p>
-          </button>
-        </a>
-      </div>
-    );
-  }
-}
+const Project = ({ user, name }) => {
+  const [description, setDescription] = useState('');
+  const [htmlUrl, setHtmlUrl] = useState('');
+
+  getRepositoryInformation(user, name)
+    .then((response) => response.json())
+    .then(({ description, html_url }) => {
+      setDescription(description);
+      setHtmlUrl(html_url);
+    });
+
+  return (
+    <div className="Project">
+      <a href={htmlUrl} target="_blank" rel="noopener noreferrer">
+        <button type="button">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </button>
+      </a>
+    </div>
+  );
+};
 
 export default Project;
--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './style.scss';
 
-const Social = ({ link, icon }) => {
-  return (
-    <div className="Social">
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        <img src={icon} className="Social-icon" alt="icon" />
-      </a>
-    </div>
-  );
-};
+const Social = ({ link, icon }) => (
+  <div className="Social">
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <img src={icon} className="Social-icon" alt="icon" />
+    </a>
+  </div>
+);
 
 export default Social;
