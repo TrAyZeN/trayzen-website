390af0d9f04d03a33aaf1f6348f12de9746454c2
TrAyZeN <1810leo@gmail.com>
2021-02-01 18:06:27 +0100

    Add social links

---
+++ src/components/SocialButton/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.scss';
+
+type Props = {
+  icon: string;
+  link: string;
+  alt?: string;
+};
+
+const SocialButton: React.FC<Props> = ({ link, icon, alt }: Props) => (
+  <div className="social-button">
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <img src={icon} className="icon" alt={alt} />
+    </a>
+  </div>
+);
+
+SocialButton.defaultProps = {
+  alt: 'icon',
+};
+
+export default SocialButton;
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
+import SocialButton from '../../components/SocialButton';
 import './style.scss';
+
+import EmailIcon from '../../assets/email.svg';
+import GithubIcon from '../../assets/github.svg';
+import GitlabIcon from '../../assets/gitlab.svg';
+import LinkedinIcon from '../../assets/linkedin.svg';
 
 const About: React.VFC = () => (
   <div className="about">
@@ -12,6 +18,30 @@ const About: React.VFC = () => (
       code, learning TDD, and other technologies or methods, I love to learn new
       things.
     </p>
+
+    <div className="social">
+      <SocialButton
+        icon={EmailIcon}
+        link="mailto:lbenitoproduction@gmail.com"
+        alt="email"
+      />
+      <SocialButton
+        icon={GithubIcon}
+        link="https://github.com/trayzen"
+        alt="github"
+      />
+      <SocialButton
+        icon={GitlabIcon}
+        link="https://gitlab.com/TrAyZeN"
+        alt="gitlab"
+      />
+      <SocialButton
+        icon={LinkedinIcon}
+        link="https://www.linkedin.com/in/leo-benito"
+        alt="linkedin"
+      />
+    </div>
+
     <NavBar items={[{ text: 'Home', to: '/' }, 'Projects']} />
   </div>
 );
