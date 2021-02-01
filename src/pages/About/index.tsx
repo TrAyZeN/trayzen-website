3648b63d3e5fa02a66bffca081537d05b2b7f3ff
TrAyZeN <1810leo@gmail.com>
2021-02-01 15:31:39 +0100

    Add about text

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import NavBar from '../../components/NavBar';
+import './style.scss';
 
 const About: React.VFC = () => (
   <div className="about">
-    <h1>About</h1>
+    <h1>About me</h1>
+    <p>
+      Hi I am TrayZeN, I am Interested in Mathematics, Engineering, Electronics,
+      6+ years of programming. Investigating old source code, hacking and
+      practicing competitive programming in my spare time. Also, producing clean
+      code, learning TDD, and other technologies or methods, I love to learn new
+      things.
+    </p>
+    <NavBar items={[{ text: 'Home', to: '/' }, 'Projects']} />
   </div>
 );
 
