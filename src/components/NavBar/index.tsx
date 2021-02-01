d7652097bbaeaa5bcc5bcf260b4eb237972bde12
TrAyZeN <1810leo@gmail.com>
2021-02-01 14:31:24 +0100

    Add NavBar component

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './style.scss';
+
+type Props = {
+  items: (NavItem | string)[];
+};
+
+type NavItem = {
+  text: string;
+  to: string;
+};
+
+/*
+ * Checks if a given item is of type NavItem.
+ */
+const isNavItem = (item: NavItem | string) =>
+  (item as NavItem).text !== undefined;
+
+/*
+ * Converts an item of type NavItem or string into an <a> element.
+ */
+const itemToAElement = (item: NavItem | string) => {
+  if (isNavItem(item)) {
+    const i = item as NavItem;
+    return (
+      <a href={i.to} key={i.text}>
+        {i.text}
+      </a>
+    );
+  }
+
+  const i = item as string;
+  return (
+    <a href={`/${i.toLowerCase()}`} key={i}>
+      {i}
+    </a>
+  );
+};
+
+const NavBar: React.FC<Props> = ({ items }: Props) => (
+  <div className="navbar">{items.map(itemToAElement)}</div>
+);
+
+export default NavBar;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.scss';
 
+import NavBar from '../../components/NavBar';
 import ProfilePicture from '../../assets/profile.png';
 
 const Home: React.VFC = () => (
@@ -9,10 +10,7 @@ const Home: React.VFC = () => (
       <img className="profile-picture" src={ProfilePicture} alt="Profile" />
       <p>Hi I am TrAyZeN</p>
     </div>
-    <div className="navbar">
-      <a href="/projects">Projects</a>
-      <a href="/about">About</a>
-    </div>
+    <NavBar items={['Projects', 'About']} />
   </div>
 );
 
