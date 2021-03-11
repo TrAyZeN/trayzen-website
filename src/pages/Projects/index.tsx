714896b1e3d0c2fa1b98368744966a7267668766
TrAyZeN <1810leo@gmail.com>
2021-03-11 08:48:46 +0100

    Add technologies to project page

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import NavBar from '../../components/NavBar';
 import './style.scss';
 
 import Prism from '../../assets/prism.png';
 import Ora from '../../assets/ora.png';
 import EquipageSolidaire from '../../assets/equipage_solidaire.png';
+import CLogo from '../../assets/c.svg';
+import CSharpLogo from '../../assets/csharp.svg';
+import NodeJsLogo from '../../assets/nodejs.svg';
+import ReactLogo from '../../assets/react.svg';
+import SassLogo from '../../assets/sass.svg';
+import MongoDbLogo from '../../assets/mongodb.svg';
+import RedisLogo from '../../assets/redis.svg';
+import DockerLogo from '../../assets/docker.svg';
 
 const Projects: React.VFC = () => (
   <div className="projects">
     <h1>Projects</h1>
+    <NavBar items={[{ text: 'Home', to: '/' }, 'Project', 'About']} />
     <div className="projects-container">
       <div className="project">
         <h2>Prism</h2>
@@ -19,6 +29,9 @@ const Projects: React.VFC = () => (
           <div className="image-container">
             <img src={Prism} alt="Prism" />
             <div className="overlay">
+              <div className="logo-container">
+                <img src={CLogo} alt="C language" />
+              </div>
               <p style={{ color: 'white' }}>Click to learn more ;)</p>
             </div>
           </div>
@@ -38,6 +51,9 @@ const Projects: React.VFC = () => (
           <div className="image-container">
             <img src={Ora} alt="ORA" />
             <div className="overlay">
+              <div className="logo-container">
+                <img src={CSharpLogo} alt="C Sharp" />
+              </div>
               <p>Click to learn more ;)</p>
             </div>
           </div>
@@ -54,6 +70,14 @@ const Projects: React.VFC = () => (
           <div className="image-container">
             <img src={EquipageSolidaire} alt="Equipage Solidaire" />
             <div className="overlay">
+              <div className="logo-container">
+                <img src={NodeJsLogo} alt="Node.JS" />
+                <img src={ReactLogo} alt="React.JS" />
+                <img src={SassLogo} alt="Sass" />
+                <img src={MongoDbLogo} alt="MongoDB" />
+                <img src={RedisLogo} alt="Redis" />
+                <img src={DockerLogo} alt="Docker" />
+              </div>
               <p>Click to learn more ;)</p>
             </div>
           </div>
@@ -61,6 +85,11 @@ const Projects: React.VFC = () => (
         <p>
           A website for L&apos;Equipage Solidaire a french charity organization.
         </p>
+      </div>
+      <div className="attributions">
+        Icons by{' '}
+        <a href="https://iconscout.com/contributors/icon-mafia">Icon Mafia</a>{' '}
+        on <a href="https://iconscout.com">Iconscout</a>
       </div>
     </div>
   </div>
