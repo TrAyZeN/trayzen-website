d0a08645a395108121f9a70f113f22fa30c34b3a
TrAyZeN <1810leo@gmail.com>
2022-03-27 23:43:05 +0200

    Add new projects

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import NavBar from '../../components/NavBar';
 import './style.scss';
 
+import MyMips from '../../assets/my_mips.png';
+import CrabRt from '../../assets/crab-rt.png';
 import Prism from '../../assets/prism.webp';
 import Ora from '../../assets/ora.webp';
 import EquipageSolidaire from '../../assets/equipage_solidaire.webp';
 import GottaGoCatch from '../../assets/gottagocatch.webp';
 
+import RustLogo from '../../assets/rust.svg';
 import CLogo from '../../assets/c.svg';
 import CSharpLogo from '../../assets/csharp.svg';
 import NodeJsLogo from '../../assets/nodejs.svg';
@@ -22,6 +25,105 @@ const Projects: React.VFC = () => (
     <NavBar items={[{ text: 'Home', to: '/' }, 'Projects', 'About']} />
 
     <div className="projects-container">
+      <div className="project">
+        <h2>k</h2>
+        <h3>Feb. 2022 - Mar. 2022</h3>
+        <a
+          href="https://github.com/trayzen"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="image-container">
+            <img src={Prism} alt="Prism" width="1243" height="749" />
+            <div className="overlay">
+              <div className="logo-container">
+                <img src={CLogo} alt="C language" width="60" height="60" />
+              </div>
+              <p style={{ color: 'white' }}>Click to learn more ;)</p>
+            </div>
+          </div>
+        </a>
+        <p className="description">
+          k is a small x86 kernel. It features memory segmentation, syscalls,
+          ELF loading, iso9660 filesystem. Multiple drivers are implemented such
+          as: 8250 UART driver, 8259A PIC driver, 8042 keyboard driver, 8254 PIT
+          driver, ATAPI driver.
+        </p>
+      </div>
+      <div className="project">
+        <h2>my_mips</h2>
+        <h3>Dec. 2021 - Jan. 2022</h3>
+        <a
+          href="https://github.com/trayzen"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="image-container">
+            <img src={MyMips} alt="my_mips" width="1243" height="749" />
+            <div className="overlay">
+              <div className="logo-container">
+                <img src={CLogo} alt="C language" width="60" height="60" />
+              </div>
+              <p style={{ color: 'white' }}>Click to learn more ;)</p>
+            </div>
+          </div>
+        </a>
+        <p className="description">
+          my_mips is a MIPS-I emulator. My implementation supports ELF loading
+          and implements JIT compilation of MIPS instructions into x86
+          instructions.
+        </p>
+      </div>
+      <div className="project">
+        <h2>42sh</h2>
+        <h3>Nov. 2021 - Dec. 2021</h3>
+        <a
+          href="https://github.com/trayzen"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="image-container">
+            <img src={Prism} alt="Prism" width="1243" height="749" />
+            <div className="overlay">
+              <div className="logo-container">
+                <img src={CLogo} alt="C language" width="60" height="60" />
+              </div>
+              <p style={{ color: 'white' }}>Click to learn more ;)</p>
+            </div>
+          </div>
+        </a>
+        <p className="description">42sh is a POSIX shell implementation.</p>
+      </div>
+      <div className="project">
+        <h2>crab-rt</h2>
+        <h3>Dec. 2020 - Now</h3>
+        <a
+          href="https://github.com/trayzen/crab-rt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="image-container">
+            <img src={CrabRt} alt="crab-rt" width="1243" height="749" />
+            <div className="overlay">
+              <div className="logo-container">
+                <img
+                  src={RustLogo}
+                  alt="Rust language"
+                  width="60"
+                  height="60"
+                />
+              </div>
+              <p style={{ color: 'white' }}>Click to learn more ;)</p>
+            </div>
+          </div>
+        </a>
+        <p className="description">
+          crab-rt is small Path Tracer running on CPU. It implements sphere
+          shapes, lambertian material, specular reflection material, lights and
+          more. It is multithreaded and implements Bounded Volume Hierarchy
+          (BVH) as an acceleration structure.
+        </p>
+      </div>
       <div className="project">
         <h2>Prism</h2>
         <h3>Sep. 2020 - Dec. 2020</h3>
@@ -41,8 +143,9 @@ const Projects: React.VFC = () => (
           </div>
         </a>
         <p className="description">
-          Prism is a software that allows you to convert an image of text into
-          text data.
+          Prism is an OCR. It is a software that allows you to convert an image
+          of text into text data. Prism features a CLI and GUI application. A
+          neural network is used under the hood to recognize characters.
         </p>
       </div>
       <div className="project">
@@ -75,6 +178,8 @@ const Projects: React.VFC = () => (
         </a>
         <p className="description">
           A website for L&apos;Equipage Solidaire a french charity organization.
+          Note that the backend is not hosted anymore and the domain name has
+          not been renewed.
         </p>
       </div>
       <div className="project">
